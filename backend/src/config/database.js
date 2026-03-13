02f176c71c54069c120c36c9b4da7733e6c32983
Aftab Khan <aftabkhan11480@gmail.com>
2026-03-14 00:38:25 +0530

    database.js

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,21 +1,10 @@
-const { Pool } = require('pg');
-const { logger } = require('../utils/logger');
+const { Pool } = require("pg")
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : false,
-  max: 10,
-  idleTimeoutMillis: 30_000,
-  connectionTimeoutMillis: 5_000,
-});
+  ssl: {
+    rejectUnauthorized: false
+  }
+})
 
-pool.on('connect', () => logger.debug('DB connection acquired'));
-pool.on('error',   (err) => logger.error('DB pool error:', err.message));
-
-// Convenience wrapper
-const db = {
-  query: (text, params) => pool.query(text, params),
-  pool,
-};
-
-module.exports = db;
+module.exports = pool
